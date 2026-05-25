331e1e4c146a7eec2cd7fb95080175553011e585
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-05-24 21:29:30 -0500

    Cálculo del Binomial para la cantidad minima de caminos en la grilla

---
+++ Modules/GridPaths/Algorithms/BinomialCalc.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+namespace Problemas_de_Conteo.Modules.GridPaths.Algorithms
+{
+	public static class BinomialCalc
+	{
+		// Fucnion del calculo binomial (a+b b)
+		public static BigInteger CalculateBin(int n, int k)
+		{
+			if (k < 0 || k > n)
+				return 0;
+
+			if (k == 0 || k == n)
+				return 1;
+
+			k = Math.Min(k, n-k);
+
+			BigInteger result = 1;
+
+			for (int i=1; i <= k; i++)
+			{
+				result *= n - (k - i);
+				result /= i;
+			}
+			return result;
+		}
+	}
+}
