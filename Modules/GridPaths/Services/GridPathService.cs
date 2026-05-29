58553d037572ddc071a6866e4865cb0993c0277a
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-05-29 09:47:01 -0500

    Calcula el camino minimo simple

--- Modules/GridPaths/Services/GridPathService.cs
+++ Modules/GridPaths/Services/GridPathService.cs
@@ -5,6 +5,7 @@ namespace Problemas_de_Conteo.Modules.GridPaths.Services
 {
 	public class GridPathService
 	{
+		// Camino base (sin puntos especialess)
 		public BigInteger CalculateBasicPath(int a, int b)
 		{
 			return BinomialCalc.CalculateBin(a + b, b); 
