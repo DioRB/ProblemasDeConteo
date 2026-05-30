311303334383782a64d0a0509515f7ea450fc7d7
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-05-30 13:22:33 -0500

    Servicios y arreglo general

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Problemas_de_Conteo;
 using Problemas_de_Conteo.Modules.GridPaths.Services;
+using Problemas_de_Conteo.Modules.PasswordCounter.Services;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -15,6 +16,8 @@ builder.Services.AddScoped<GridPathService>();
 builder.Services.AddScoped<GridGeneratorService>();
 builder.Services.AddScoped<GridDynamicService>();
 
-
+//Servicios para las contraseñas
+builder.Services.AddScoped<PasswordCountService>();
+builder.Services.AddScoped<PasswordValidatorService>();
 
 await builder.Build().RunAsync();
