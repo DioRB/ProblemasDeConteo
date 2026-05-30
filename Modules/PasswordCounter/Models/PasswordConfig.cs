eb6236bb4ce9935305f500610e9f53c5ec39ed9e
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-05-30 13:24:25 -0500

    Modelo para la estructura general de la contraseña

---
+++ Modules/PasswordCounter/Models/PasswordConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Problemas_de_Conteo.Modules.PasswordCounter.Models
+{
+	// Modelo general
+	public class PasswordConfig
+	{
+		public int Length { get; set; }
+		public int AlphabetSize { get; set; }
+		public bool AllowRepetition { get; set; }
+        public List<CharacterGroup> Groups { get; set; } = new();
+    }
+}
