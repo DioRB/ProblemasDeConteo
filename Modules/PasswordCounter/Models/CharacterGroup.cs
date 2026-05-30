1c7fbafa3e7717de9194854611a0f1180a554e45
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-05-30 13:23:17 -0500

    Modelo de los grupos variables para agregar

---
+++ Modules/PasswordCounter/Models/CharacterGroup.cs
@@ -0,0 +1,12 @@
+﻿namespace Problemas_de_Conteo.Modules.PasswordCounter.Models
+{
+    // Modelo para los grupos personalizados
+    public class CharacterGroup
+    {
+        public string Name { get; set; } = "";
+
+        public int Size { get; set; }
+
+        public bool Required { get; set; }
+    }
+}
